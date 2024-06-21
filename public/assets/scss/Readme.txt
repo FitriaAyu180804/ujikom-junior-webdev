76578c3a35dd37f52e11ab3e51d450d8e91b76ec
Fitria <fitria180804@gmail.com>
2024-06-22 01:24:46 +0700

    [FEAT] Dashboard

--- public/assets/scss/Readme.txt
+++ public/assets/scss/Readme.txt
@@ -1,2 +1,2 @@
 The .scss (Sass) files are only available in the pro version.
-You can buy it from: https://bootstrapmade.com/scaffold-bootstrap-metro-style-template/
+You can buy it from: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,30 @@
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+        <li class="nav-item">
+            <a class="nav-link " href="{{ route('dashboard')}}">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="fas fa-users fa-fw"></i><span>Pegawai</span><i></i>
+            </a>
+
+
+        <li class="nav-heading">Pages</li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="pages-login.html">
+                <i class="fas fa-sign-out-alt"></i>
+                <span>Sign Out</span>
+            </a>
+        </li><!-- End Login Page Nav -->
+
+    </ul>
+
+</aside><!-- End Sidebar-->
