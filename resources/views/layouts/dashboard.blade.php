4d585a4581f954514f4f9e4866d2737f28448e50
Fitria <fitria180804@gmail.com>
2024-06-25 06:53:20 +0700

    [FEAT] partials

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,83 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+@include('layouts.partials._head')
 
-<head>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-    <title>Dashboard</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
-
-    <!-- Favicons -->
-    <link href="{{ asset('') }}asset/img/favicon.png" rel="icon">
-    <link href="{{ asset('') }}asset/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-    <!-- Google Fonts -->
-    <link href="https://fonts.gstatic.com" rel="preconnect">
-    <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-        rel="stylesheet">
-
-    <!--Font Awesome-->
-    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-
-    <!-- Vendor CSS Files -->
-    <link href="{{ asset('') }}asset/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="{{ asset('') }}asset/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="{{ asset('') }}asset/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="{{ asset('') }}asset/vendor/quill/quill.snow.css" rel="stylesheet">
-    <link href="{{ asset('') }}asset/vendor/quill/quill.bubble.css" rel="stylesheet">
-    <link href="{{ asset('') }}asset/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="{{ asset('') }}asset/vendor/simple-datatables/style.css" rel="stylesheet">
-
-    <!-- Template Main CSS File -->
-    <link href="{{ asset('') }}asset/css/style.css" rel="stylesheet">
-    @stack('css')
-</head>
-
-<body>
     @include('layouts.partials.sidebar')
 
     @include('layouts.partials.header')
 
     <main id="main" class="main">
-
         <div class="container-fluid py-4">
             @yield('content')
         </div>
     </main><!-- End #main -->
 
-    <!-- ======= Footer ======= -->
-    {{-- <footer id="footer" class="footer">
-        <div class="copyright">
-            &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
-        </div>
-        <div class="credits">
-            <!-- All the links in the footer should remain intact. -->
-            <!-- You can delete the links only if you purchased the pro version. -->
-            <!-- Licensing information: https://bootstrapmade.com/license/ -->
-            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-        </div>
-    </footer><!-- End Footer --> --}}
+    @include('layouts.partials.footer')
+    @include('layouts.partials.loader')
 
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-            class="bi bi-arrow-up-short"></i></a>
-
-    <!-- Vendor JS Files -->
-    <script src="{{ asset('') }}assets/vendor/apexcharts/apexcharts.min.js"></script>
-    <script src="{{ asset('') }}assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('') }}assets/vendor/chart.js/chart.umd.js"></script>
-    <script src="{{ asset('') }}assets/vendor/echarts/echarts.min.js"></script>
-    <script src="{{ asset('') }}assets/vendor/quill/quill.js"></script>
-    <script src="{{ asset('') }}assets/vendor/simple-datatables/simple-datatables.js"></script>
-    <script src="{{ asset('') }}assets/vendor/tinymce/tinymce.min.js"></script>
-    <script src="{{ asset('') }}assets/vendor/php-email-form/validate.js"></script>
-
-    <!-- Template Main JS File -->
-    <script src="{{ asset('') }}assets/js/main.js"></script>
-    @stack('js')
-</body>
-
-</html>
+@include('layouts.partials._foot')
---
+++ resources/views/layouts/partials/_head.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Dashboard</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="{{ asset('') }}asset/img/favicon.png" rel="icon">
+    <link href="{{ asset('') }}asset/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!--Font Awesome-->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('') }}asset/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('') }}asset/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="{{ asset('') }}asset/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="{{ asset('') }}asset/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="{{ asset('') }}asset/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="{{ asset('') }}asset/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="{{ asset('') }}asset/vendor/simple-datatables/style.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{ asset('') }}asset/css/style.css" rel="stylesheet">
+    @stack('css')
+</head>
+<body>
